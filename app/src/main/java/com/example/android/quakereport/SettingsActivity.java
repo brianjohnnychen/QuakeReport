b80c03fdc531c8fc5f211d37be07bb0b424ae8ea
Brian Chen <brianjohnnychen@gmail.com>
2018-03-24 23:37:35 -0400

    added preferences to allow user to set minimum magnitude of earthquakes to show

---
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -0,0 +1,63 @@
+package com.example.android.quakereport;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by brian on 3/24/2018.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            /*
+            Update the preference summary when the settings activity is launched.
+            Given the key of a preference, we can use PreferenceFragment's findPreference() method to get the Preference object,
+            and setup the preference using a helper method called bindPreferenceSummaryToValue().
+            * */
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
+        }
+
+        /**
+         * when a single Preference has been changed by the user and is about to be saved,
+         * the onPreferenceChange() method will be invoked with the key of the preference that was changed.
+         * Note that this method returns a boolean,
+         * which allows us to prevent a proposed preference change by returning false.
+         */
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        /*
+        * helper method to set the current EarthquakePreferenceFragment instance as the listener on each preference.
+        * We also read the current value of the preference stored in the SharedPreferences on the device,
+        * and display that in the preference summary (so that the user can see the current value of the preference).
+        * */
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+    }
+}
